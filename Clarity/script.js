80d7266756b4c6e67613d0d08ef446115af7e3a3
AC2100 <alisonec2100@gmail.com>
2021-04-18 03:01:34 -0700

    Update script.js

--- Clarity/script.js
+++ Clarity/script.js
@@ -4,3 +4,12 @@ function f(){
 //For the breaking of lines, so that they can repeat
 var breakItem = document.getElementById("break");
 breakItem.innerHTML = "<br>".repeat(10);
+
+//to help get input form contact
+function getTextboxInput(idOfTextbox) {
+	return document.getElementById(idOfTextbox).value;
+}
+
+//function isItFilled(idOfRadioButton) {
+//	return document.getElementById(idOfRadioButton).checked;
+//}
